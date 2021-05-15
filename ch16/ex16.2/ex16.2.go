10fc9a1404c481539a3fef7cb6c9a83a61bf2a87
Jisoo <sujisu12@smartsocial.co.kr>
2021-05-15 22:39:57 +0900

    외부패키지 사용 연습 단원

---
+++ ch16/ex16.2/ex16.2.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	// "ch16/ex16.2/publicpkg"
+	"fmt"
+	"goproject/ch16/ex16.2/publicpkg"
+)
+
+func main() {
+	fmt.Println("PI:", publicpkg.PI)
+}
---
+++ ch16/ex16.2/go.mod
@@ -0,0 +1,3 @@
+module goproject/ch16/ex16.2
+
+go 1.16
